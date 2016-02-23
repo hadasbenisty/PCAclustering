a2587fb47fae6320864d95a72709cb687ba46899
hadas <hadas.benisty@gmail.com>
2016-02-23 09:19:57 +0200

    Fixing some bugs - running over k~=2 Also - embedd the samples here

--- BuildTreeViaPCAclustering.m
+++ BuildTreeViaPCAclustering.m
@@ -1,4 +1,11 @@
-function tree = BuildTreeViaPCAclustering(data, params)
+function [tree, embedding] = BuildTreeViaPCAclustering(data, params)
+
+% not using the embedding of SVNClass - embed here
+[vecs, vals] = CalcEigs(threshold(data, params.threshold), params.eigs_num);
+embedding = vecs*vals;
+if params.runOnEmbdding
+    data = embedding;
+end
 
 
 %initialize root (top-most level):
@@ -7,23 +14,27 @@ rev_tree{1}.folder_count = 1;
 rev_tree{1}.folder_sizes = N;
 rev_tree{1}.clustering = ones(1, N);
 rev_tree{1}.super_folders = [];
-
-[clustering,pc,affinity,eigVals] = svdClass({permute(data, [2 3 1])}, params.k, params.embedded, params.threshold, ...
+% not using the embedding of SVNClass - embed here
+clustering = svdClass({permute(data, [2 3 1])}, params.k, false, 0, ...
     ['Building a tree level: ' num2str(1)],params.verbose);
-rev_tree{2}.folder_count = params.k;
+rev_tree{2}.folder_count = numel(unique(clustering));
 rev_tree{2}.clustering = clustering;
-for ki = 1:params.k
+for ki = 1:rev_tree{2}.folder_count
     rev_tree{2}.folder_sizes(ki) = sum(rev_tree{2}.clustering == ki);
 end
-rev_tree{2}.super_folders = [1 1];
+rev_tree{2}.super_folders = ones(1, rev_tree{2}.folder_count);
 currLevel = 3;
 MAX_ITERS = 1e6;
 for iter = 1:MAX_ITERS
+    if params.treeDepth <= currLevel
+        break;
+    end
+    rev_tree{currLevel}.super_folders = [];
     clusters = unique(rev_tree{currLevel-1}.clustering);
     maxCluster = 0;
     for ci = 1:length(clusters)
         curr_cluster_inds2data = find(rev_tree{currLevel-1}.clustering == clusters(ci));
-        supFolders(curr_cluster_inds2data) = ci;
+%         supFolders(curr_cluster_inds2data) = ci;
         if numel(curr_cluster_inds2data) > params.min_cluster
             if params.verbose > 1
             disp(['Tree level ' num2str(currLevel) ' cluster num ' num2str(ci)]);
@@ -42,15 +53,20 @@ for iter = 1:MAX_ITERS
            rev_tree{currLevel}.clustering(curr_cluster_inds2data) = maxCluster + 1;
         end
         maxCluster = max(rev_tree{currLevel}.clustering);
-    end
-    rev_tree{currLevel}.super_folders = [];
-    for ci = 1:length(finished)
-       if  finished(ci)
+        if  finished(ci)
            rev_tree{currLevel}.super_folders = [rev_tree{currLevel}.super_folders ci];
        else
-           rev_tree{currLevel}.super_folders = [rev_tree{currLevel}.super_folders ci ci];
+           rev_tree{currLevel}.super_folders = [rev_tree{currLevel}.super_folders ci*ones(1, numel(unique(clustering)))];
        end
     end
+    
+%     for ci = 1:length(finished)
+%        if  finished(ci)
+%            rev_tree{currLevel}.super_folders = [rev_tree{currLevel}.super_folders ci];
+%        else
+%            rev_tree{currLevel}.super_folders = [rev_tree{currLevel}.super_folders ci*ones(1, numel(unique(clustering)))];
+%        end
+%     end
     rev_tree{currLevel}.folder_count = numel(unique(rev_tree{currLevel}.clustering));
     for ki = 1:rev_tree{currLevel}.folder_count
         rev_tree{currLevel}.folder_sizes(ki) = sum(rev_tree{currLevel}.clustering == ki);
@@ -63,11 +79,13 @@ end
 % last level - each data point is a leaf
 
 
-rev_tree{currLevel}.folder_count = N;
-rev_tree{currLevel}.folder_sizes = ones(1,N);
-rev_tree{currLevel}.clustering = 1:N;
-rev_tree{currLevel}.super_folders = rev_tree{currLevel-1}.clustering;
+rev_tree{end+1}.folder_count = N;
+rev_tree{end}.folder_sizes = ones(1,N);
+rev_tree{end}.clustering = 1:N;
+rev_tree{end}.super_folders = rev_tree{currLevel-1}.clustering;
 
 tree = fliplr(rev_tree);
-
+for k=1:length(tree)
+    tree{k}.super_folders = transpose(tree{k}.super_folders(:));
+end
 
