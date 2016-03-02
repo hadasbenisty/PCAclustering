6c3357bf82ad638e69b05b9a60e9ba7957872f4b
hadas <hadas.benisty@gmail.com>
2016-03-02 08:26:43 +0200

    changing the field 'k' to 'splitsNum' - a meaningful name

--- svdClassWrapper.m
+++ svdClassWrapper.m
@@ -1,5 +1,5 @@
 function clustering = svdClassWrapper(data, params, inds)
 
 
-clustering = svdClass({permute(data(inds, inds), [2 3 1])}, params.k, false, 0, ...
-    ['Building a tree level: ' num2str(1)],params.verbose);
+clustering = svdClass({permute(data(inds, inds), [2 3 1])}, params.splitsNum, false, 0, ...
+    ['Building a tree level: ' num2str(1)],params.verbose > 1);
