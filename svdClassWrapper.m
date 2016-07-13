0318c8435fb97ebb01a38e6f93ed748bd7feac8a
hadasbenisty <hadas.benisty@gmail.com>
2016-07-13 14:30:56 +0300

    Give the data and not indices. Take care of indices ourside

--- svdClassWrapper.m
+++ svdClassWrapper.m
@@ -1,5 +1,5 @@
-function clustering = svdClassWrapper(data, splitsNum, params, inds)
+function clustering = svdClassWrapper(data, splitsNum, params)
 
 
-clustering = svdClass({permute(data(inds, inds), [2 3 1])}, splitsNum, false, 0, ...
+clustering = svdClass({permute(data, [2 3 1])}, splitsNum, false, 0, ...
     ['Building a tree level: ' num2str(1)],0);
